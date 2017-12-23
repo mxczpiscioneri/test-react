9d6b7c368afba5913072713efb99bb14d7ebf8a9
piscioneri <you@example.com>
2017-12-23 16:05:22 -0200

    Adicionar imagem default no Testimonial

--- src/client/components/Testimonial/Testimonial.js
+++ src/client/components/Testimonial/Testimonial.js
@@ -2,11 +2,13 @@ import React from 'react'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
 import Slider from 'react-slick'
+import ImageFallback from 'react-image-fallback'
 import { Container, Row, Col } from '../Grid/Grid'
 import icon from './icone-quote.svg'
 import imgUser01 from './user01.png'
 import imgUser02 from './user02.png'
 import imgUser03 from './user03.png'
+import imgFallback from './userDefault.png'
 import styles from './testimonial.css'
 
 const Testimonial = props => {
@@ -14,12 +16,18 @@ const Testimonial = props => {
   const listTestimonial = [
     { name: "Miguel Alves Barros", picture: imgUser01, message: "Eu fui muito bem atendido e a proposta oferecida foi muito boa. Eu recomendo e faria novamente." },
     { name: "Douglas Cavalcanti Souza", picture: imgUser02, message: "Eu fui muito bem atendido e a proposta oferecida foi muito boa. Eu recomendo e faria novamente." },
-    { name: "Raissa Fernandes Pereira", picture: imgUser03, message: "Eu fui muito bem atendido e a proposta oferecida foi muito boa. Eu recomendo e faria novamente." },
+    { name: "Raissa Fernandes Pereira", message: "Eu fui muito bem atendido e a proposta oferecida foi muito boa. Eu recomendo e faria novamente." },
   ]
 
   const testimonials = listTestimonial.map((testimonial, i) => (
     <section key={i} className={styles.testimonial}>
-      <img className={styles.picture} src={testimonial.picture} alt={testimonial.name} />
+      <ImageFallback
+        src={testimonial.picture}
+        fallbackImage={imgFallback}
+        alt={testimonial.name}
+        useLayerForClickAway={true}
+      />
+
       <p className={styles.message}>"{testimonial.message}"</p>
       <p className={styles.name}>{testimonial.name}</p>
       <img className={styles.icon} src={icon} alt={testimonial.title} />
