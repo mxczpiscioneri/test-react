b7bb6c40bb249570c1af9320702de7da9c2341ae
Matheus Piscioneri <mxczpiscioneri@gmail.com>
2017-12-29 12:56:09 -0200

    Plans mobile

--- src/client/components/Plans/Plans.js
+++ src/client/components/Plans/Plans.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
-import { Container, Row, Col } from '../Grid/Grid'
+import { Container, Row, Col, Hidden } from '../Grid/Grid'
 import Cars from './Cars/Cars'
 import styles from './plans.css'
 import Star from './icone-estrela.svg';
@@ -24,23 +24,45 @@ class Plans extends React.Component {
     return (
       <section className={styles.container}>
         <Container>
-          <Row>
-            <Col xs={12}>
-              <h3 className={styles.title}>Resultado da sua simulação</h3>
-            </Col>
-          </Row>
-          <Row>
-            <Col xs={12} sm={5} offset={{ sm: 1 }}>
-              <p className={styles.textResult}>Você terá uma carta de crédito de R$ <span>45.000,00</span> pagando apenas <span>R$ 1.200</span> por <span>72</span> meses.</p>
-              <p className={styles.textResult}>Caso queira pagar só <span className={styles.green}>R$ 700</span> nos primeiros meses, não deixe de optar pelo plano Flex!</p>
-              <p className={styles.textResult}>Valor do veículo R$ 45.000 <span className={styles.light}>Prazo 72 Meses</span></p>
-              <button onClick={this.submitForm} className={styles.btn}>TENHO INTERESSE</button>
-            </Col>
-            <Col xs={12} sm={4}>
-              <p className={styles.suggestions}>Sugestão de <span>veículos</span> que você poderá comprar com a carta escolhida.</p>
-              <Cars />
-            </Col>
-          </Row>
+          <Hidden sm md lg xl>
+            <Row>
+              <Col xs={6} className={styles.normal}>
+                <label>Parcela normal</label>
+                <span>72 x R$ 1.300</span>
+              </Col>
+              <Col xs={6} className={styles.flex}>
+                <label>Parcela flex</label>
+                <span>72 x R$ 900,00</span>
+              </Col>
+              <Col xs={12}>
+                <button onClick={this.submitForm} className={styles.btn}>TENHO INTERESSE</button>
+                <hr className={styles.separator} />
+              </Col>
+              <Col xs={12}>
+                <h3 className={styles.titleSuggestions}>Sugestões de veículos que você poderá <span>comprar</span>*</h3>
+                <Cars />
+              </Col>
+            </Row>
+          </Hidden>
+          <Hidden xs>
+            <Row>
+              <Col xs={12}>
+                <h3 className={styles.title}>Resultado da sua simulação</h3>
+              </Col>
+            </Row>
+            <Row>
+              <Col xs={12} sm={5} offset={{ sm: 1 }}>
+                <p className={styles.textResult}>Você terá uma carta de crédito de R$ <span>45.000,00</span> pagando apenas <span>R$ 1.200</span> por <span>72</span> meses.</p>
+                <p className={styles.textResult}>Caso queira pagar só <span className={styles.green}>R$ 700</span> nos primeiros meses, não deixe de optar pelo plano Flex!</p>
+                <p className={styles.textResult}>Valor do veículo R$ 45.000 <span className={styles.light}>Prazo 72 Meses</span></p>             
+                <button onClick={this.submitForm} className={styles.btn}>TENHO INTERESSE</button>
+              </Col>
+              <Col xs={12} sm={4}>
+                <p className={styles.suggestions}>Sugestão de <span>veículos</span> que você poderá comprar com a carta escolhida.</p>
+                <Cars />
+              </Col>
+            </Row>
+          </Hidden>
         </Container>
       </section>
     )
